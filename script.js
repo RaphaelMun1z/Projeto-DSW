a9764608ecf911e7f0558adc57535fc362cafd7f
RaphaelMun1z <93897942+RaphaelMun1z@users.noreply.github.com>
2023-03-08 22:02:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.navbar').toggleClass("navigating", ($(window).scrollTop() > 0));
+        $('aside').toggleClass("navigating", ($(window).scrollTop() > 0));
+     });
+});
